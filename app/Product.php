a589eff4fdb013197ea8fe96211c44ee1c6a472c
Antoni March Marti <marchmarti.antoni@gmail.com>
2020-03-10 17:12:28 +0100

    create models of category and products

---
+++ app/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $filliable = ['name', 'price', 'description'];
+
+    public function category()
+    {
+        return $this->belongsTo('App/Category');
+    }
+}
